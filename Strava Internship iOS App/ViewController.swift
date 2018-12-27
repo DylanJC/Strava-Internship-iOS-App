30ffc1b4ed362e495135c51682bb727ab8c50d1d
DylanJC <dcunningham518@gmail.com>
2018-12-27 12:31:23 -0700

    Initial Commit

---
+++ Strava Internship iOS App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Strava Internship iOS App
+//
+//  Created by Dylan Cunningham on 12/27/18.
+//  Copyright © 2018 Dylan Cunningham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
